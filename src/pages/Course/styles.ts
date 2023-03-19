cf2a328ec79e4151e6ac56da2470b9e5e9f72252
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2023-03-18 21:33:54 -0300

    refactor: add margin top of default footer

--- src/pages/Course/styles.ts
+++ src/pages/Course/styles.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const CourseContainer = styled.div`
-  padding-bottom: 1rem;
-
   display: flex;
   flex-direction: column;
   justify-content: center;
--- src/pages/Journey/styles.ts
+++ src/pages/Journey/styles.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const JourneyContainer = styled.div`
-  padding-bottom: 1rem;
-
   display: flex;
   flex-direction: column;
   justify-content: center;
