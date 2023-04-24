3559ab6cf2a839dbb5862e73d08329ee4c415a0a
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2023-04-24 16:36:36 -0300

    feat: add Google Analytics

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,26 @@
 import logoSVG from '@assets/logo.svg';
 
 import { FooterContainer, FooterContent } from './styles';
+import { useAnalyticsEventTracker } from '@/hooks/useAnalyticsEventTracker';
 
 export default function Footer() {
+  const gaEventTracker = useAnalyticsEventTracker('Contact us');
   return (
     <FooterContainer>
       <FooterContent>
         <img src={logoSVG} alt="" />
 
         <span>
-          2023 © Todos os direitos reservados. Lucas Cavalcante Silva.
+          2023 © Todos os direitos reservados. Lucas Cavalcante Silva
+          <a href="#" onClick={() => gaEventTracker('call')}>
+            Call Us
+          </a>
+          <a
+            href="mailto:lucas.cavalcante.dev@icloud.com"
+            onClick={() => gaEventTracker('email')}
+          >
+            Write to us
+          </a>
         </span>
       </FooterContent>
     </FooterContainer>
