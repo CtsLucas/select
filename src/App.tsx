b37b96a8ba7e820697481c3f2332c2fe23360042
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2023-04-24 17:30:36 -0300

    feat: remove Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ReactGA from 'react-ga';
 
 import { Router } from '@/Router';
 import { GlobalStyle } from '@styles/global';
@@ -12,9 +11,6 @@ import { ContentsProvider } from '@contexts/ContentsContext';
 import { AuthProvider } from '@contexts/AuthContext';
 
 export function App() {
-  const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
-  ReactGA.initialize(TRACKING_ID);
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
--- src/hooks/useAnalyticsEventTracker.tsx
+++
@@ -1,8 +0,0 @@
-import ReactGA from 'react-ga';
-
-export function useAnalyticsEventTracker(category = 'Blog category') {
-  const eventTracker = (action = 'test action', label = 'test label') => {
-    ReactGA.event({ category, action, label });
-  };
-  return eventTracker;
-}
